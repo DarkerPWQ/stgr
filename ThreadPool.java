cba3ed0bc0f83e2e9b57068bec94698d8f3505d0
puwenqiang <wenqiang_pu@xinyan.com>
2017-08-31 17:59:45 +0800

    dev 1

--- ThreadPool.java
+++ ThreadPool.java
@@ -9,6 +9,7 @@ import java.util.concurrent.Executors;
  * @Modified By：
  */
 mster 5
+dev 1
 public class ThreadPool implements Runnable{
     ExecutorService executorService = Executors.newFixedThreadPool(2);
     @Override
